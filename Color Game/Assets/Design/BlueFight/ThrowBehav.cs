94bb15235a49d99a65d1a57f5f5f28520858e7b2
Thandiwe Brown <thanbrown24@gmail.com>
2019-12-02 09:24:11 -0500

    blue boss level added

--- Color Game/Assets/Design/BlueFight/ThrowBehav.cs
+++ Color Game/Assets/Design/BlueFight/ThrowBehav.cs
@@ -7,9 +7,10 @@ public class ThrowBehav : MonoBehaviour
     public static float count;
     public GameObject player;
     public GameObject theCam;
+    public GameObject boss;
     float totalSnowballsDestroyed;
     Transform enemy1, enemy2, enemy3, enemy4;
-    bool look = false, enemyCycleEnd = false;
+    bool look = false, enemyCycleEnd = false, shakeItUp = false, bossLevelActive;
     Transform[] enemies = new Transform[8];
     
     // Start is called before the first frame update
@@ -49,33 +50,44 @@ public class ThrowBehav : MonoBehaviour
             }
             if (enemyCycleEnd)
             {
-                theCam.GetComponent<CameraShake>().shakeDuration = 1f;
+                theCam.GetComponent<CameraShake>().shakeDuration = 2f;
                 theCam.GetComponent<CameraShake>().pie = true;
-
-                if(theCam.GetComponent<CameraShake>().pie)
-                {
-
-                }
-
-                if(count == 1) totalSnowballsDestroyed = 6;
-                if (count == 2) totalSnowballsDestroyed = 8;
-                if (count == 3) totalSnowballsDestroyed = 10;
-                if (count == 4) totalSnowballsDestroyed = 12;
-
-                Debug.Log("wwww " + totalSnowballsDestroyed + " " + count);
+                boss.GetComponent<BlueBoss>().idle = false;
+                boss.GetComponent<BlueBoss>().NextLevel();
                 enemyCycleEnd = false;
                 look = false;
+                shakeItUp = true;
             }
         }
-        
+
+        if (theCam.GetComponent<CameraShake>().pie && shakeItUp)
+        {
+            shakeItUp = false;
+            
+            Invoke("ShakeAndCont", 3f);
+        }
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         count++;
-        Debug.Log("ppppp " + count);
-        gameObject.GetComponent<Collider>().enabled = false;
-        MakeThrow();
+        if (count != 5)
+        {
+            Debug.Log("ppppp " + count);
+            gameObject.GetComponent<Collider>().enabled = false;
+            MakeThrow();
+        }
+        else
+        {
+            
+            bossLevelActive = true;
+            boss.GetComponent<BlueBoss>().idle = false;
+            boss.transform.Rotate(20, 0, 0);
+            boss.GetComponent<BlueBoss>().globalattack1 = true;
+            
+        }
+        
 
     }
 
@@ -92,4 +104,16 @@ public class ThrowBehav : MonoBehaviour
         }
         look = true;
     }
+
+    void ShakeAndCont()
+    {
+        boss.GetComponent<BlueBoss>().LevelBegin();
+        if (count == 1) totalSnowballsDestroyed = 6;
+        if (count == 2) totalSnowballsDestroyed = 8;
+        if (count == 3) totalSnowballsDestroyed = 10;
+        if (count == 4) totalSnowballsDestroyed = 12;
+
+        Debug.Log("wwww " + totalSnowballsDestroyed + " " + count);
+    }
+
 }
